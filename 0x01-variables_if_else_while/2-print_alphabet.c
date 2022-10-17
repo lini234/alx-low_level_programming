ee7b21442e147520954af7a52843826974ab2278
Inioluwa Adedokun <oluwaini06@yahoo.com>
2022-10-17 12:16:58 -0700

    added the 3rd file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a', a <= z, a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
