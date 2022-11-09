62967c390099b43479bf8be53f72911bf39284d1
Inioluwa Adedokun <oluwaini06@yahoo.com>
2022-11-09 13:32:13 -0800

    fic

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,8 @@ int **alloc_grid(int width, int height)
 				while (i >= 0)
 				{
 					free(array[i]);
-					i--;									}
+					i--;
+				}
 				free(array);
 				return (NULL);
 			}
@@ -40,5 +41,5 @@ int **alloc_grid(int width, int height)
 		{
 			return (NULL);
 		}
+	}
 }
-
