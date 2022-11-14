aaf753e4bf818990fc3df8317e6c8b95db494f03
Inioluwa Adedokun <oluwaini06@yahoo.com>
2022-11-14 07:35:52 -0800

    added4h file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ *free_dog - free memory of dog
+ *@d: pointer parameter to dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
